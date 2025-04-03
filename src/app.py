0c6c18b4041170e07e193259941469f63a95742d
gcgbarbosa <gcgbarbosa@gmail.com>
2025-04-03 15:42:30 +0200

    feat: add tool calling

--- src/app.py
+++ src/app.py
@@ -1,20 +1,10 @@
 import chainlit as cl
 
+from mcp_client import MCPClient
 
-from openai import AzureOpenAI
-import os
+from loguru import logger
 
-# may change in the future
-# https://learn.microsoft.com/en-us/azure/ai-services/openai/reference#rest-api-versioning
-api_version = os.environ.get("azure_openai_api_version")
-azure_endpoint = os.environ.get("azure_openai_endpoint", "")
-deployment_name = os.environ.get("azure_openai_deployment")
-llm_model = os.environ.get("azure_openai_model", "")
-azure_api_key = os.environ.get("azure_openai_key", "")
-
-client = AzureOpenAI(
-    api_version=api_version, azure_endpoint=azure_endpoint, azure_deployment=deployment_name, api_key=azure_api_key
-)
+conversation_messages = []
 
 
 def create_user_message(content: str):
@@ -24,19 +14,36 @@ def create_user_message(content: str):
     }
 
 
+client = MCPClient()
+
+
+@cl.on_chat_start
+async def on_chat_start():
+    logger.info("A new chat session has started!")
+    try:
+        await client.connect_to_server()
+    except Exception as e:
+        print(e)
+
+
 @cl.on_message
 async def main(message: cl.Message):
-    # Your custom logic goes here...
-    #
-    messages = [create_user_message(message.content)]
+    # add message received from chainlit to the history
+    formated_message = create_user_message(message.content)
+    conversation_messages.append(formated_message)
 
-    completion = client.chat.completions.create(
-        model="model",
-        messages=messages,  # type: ignore
-    )
+    response_message = await client.get_completion(conversation_messages)
 
-    response = completion.choices[-1].message.content
 
+    # add message to cl interface
     await cl.Message(
-        content=response or "",
+        content=response_message.content or "Error getting response from API",
     ).send()
+
+    # add response to history
+    conversation_messages.append(response_message)
+
+
+@cl.on_chat_end
+def on_chat_end():
+    print("The user disconnected!")
--- src/mcp_client.py
+++ src/mcp_client.py
@@ -1,11 +1,21 @@
 import asyncio
-from typing import Optional
+from typing import Any, Optional
 from contextlib import AsyncExitStack
 
-from mcp import ClientSession, StdioServerParameters
+from mcp import ClientSession, StdioServerParameters, types
 from mcp.client.stdio import stdio_client
 
-from anthropic import Anthropic
+from loguru import logger
+import json
+import os
+from openai import AzureOpenAI
+from openai.types.chat import ChatCompletionMessage
+
+api_version = os.environ.get("azure_openai_api_version")
+azure_endpoint = os.environ.get("azure_openai_endpoint", "")
+deployment_name = os.environ.get("azure_openai_deployment")
+llm_model = os.environ.get("azure_openai_model", "")
+azure_api_key = os.environ.get("azure_openai_key", "")
 
 
 class MCPClient:
@@ -13,7 +23,13 @@ class MCPClient:
         # Initialize session and client objects
         self.session: Optional[ClientSession] = None
         self.exit_stack = AsyncExitStack()
-        self.anthropic = Anthropic()
+        self.tools_openai_format = []
+        self.client = AzureOpenAI(
+            api_version=api_version,
+            azure_endpoint=azure_endpoint,
+            azure_deployment=deployment_name,
+            api_key=azure_api_key,
+        )
 
     async def connect_to_server(self, server_script_path: str = "src/mcp_server.py"):
         """Connect to an MCP server
@@ -36,11 +52,119 @@ class MCPClient:
         # List available tools
         response = await self.session.list_tools()
         tools = response.tools
-        print("\nConnected to server with tools:", [tool.name for tool in tools])
+
+        logger.debug(f"Connected to server with tools: {[tool.name for tool in tools]}")
+
+        self.tools_openai_format = [self.from_mcp_tool_to_openai(tool) for tool in tools]
+
+    def from_mcp_tool_to_openai(self, tool: types.Tool):
+        parameters = tool.inputSchema
+
+        parameters["additionalProperties"] = False
+
+        return {
+            "type": "function",
+            "function": {
+                "name": tool.name,
+                "description": (tool.description or "").strip(),
+                "parameters": parameters,
+                "strict": True,
+            },
+        }
 
     async def cleanup(self):
         """Clean up resources"""
         await self.exit_stack.aclose()
+
+    async def get_completion(self, messages: list[dict[str, Any]]):
+        if not self.session:
+            raise ValueError("Session is not initialized. Call connect_to_server() first.")
+
+        # get response from api
+        completion = self.client.chat.completions.create(
+            model="model",
+            messages=messages,  # type: ignore
+            tools=self.tools_openai_format,  # type: ignore
+        )
+
+        response = completion.choices[-1]
+
+        if response.finish_reason == "tool_calls":
+            # call tool
+            tool_calls = response.message.tool_calls
+
+            if not tool_calls:
+                raise ValueError("No tool calls found in the response")
+
+            messages.append(response.message)  # type: ignore
+
+            # TODO: do it for all calls
+
+            tool_call = tool_calls[-1]
+
+            function = tool_call.function
+
+            arguments: dict[str, Any] = json.loads(function.arguments)
+
+            func_response = await self.session.call_tool(function.name, arguments)
+
+            logger.debug(f"Tool call result: {func_response}")
+
+            text_output = (
+                "[" + ";".join([r.text for r in func_response.content if isinstance(r, types.TextContent)]) + "]"
+            )
+
+            # messages.append(
+            #     {
+            #         "role": "assistant",
+            #         "content": function.name,
+            #         "tool_calls": [
+            #             {
+            #                 "id": tool_call.id,
+            #                 "function": {
+            #                     "name": function.name,
+            #                     "arguments": function.arguments,
+            #                 },
+            #                 "type": "function",
+            #             }
+            #         ],
+            #     }
+            # )
+
+            messages.append(
+                {
+                    "role": "tool",
+                    "tool_call_id": tool_call.id,
+                    "name": function.name,
+                    "content": text_output,
+                }
+            )
+
+            # messages.append(
+            #     {
+            #         "type": "function_call_output",
+            #         "call_id": tool_call.id,
+            #         "output": text_output,
+            #     }
+            # )
+
+            # get response from api
+            completion = self.client.chat.completions.create(
+                model="model",
+                messages=messages,  # type: ignore
+                tools=self.tools_openai_format,  # type: ignore
+            )
+
+            response = completion.choices[-1]
+
+            return response.message
+
+            # return ChatCompletionMessage(role="assistant", content=f"Tool {function.name} called successfully")
+
+        logger.debug(f"Received response: {completion}")
+
+        # process response
+        return response.message
 
 
 async def main():
